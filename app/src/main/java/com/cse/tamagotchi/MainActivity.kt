2d0e7f6433916316be35662f52ca178baf50e011
Inphamiculus <thestrangerofrathe@gmail.com>
2025-09-11 12:42:17 -0700

    Made simple UI and Viewmodel connection.
    Silly button adds +10 coins to coins, which updates.

--- app/src/main/java/com/cse/tamagotchi/MainActivity.kt
+++ app/src/main/java/com/cse/tamagotchi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cse.tamagotchi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,37 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
 import com.cse.tamagotchi.ui.theme.TamagotchiTheme
+import com.cse.tamagotchi.ui.HomeScreen
+import com.cse.tamagotchi.viewmodel.HomeViewModel
 
 class MainActivity : ComponentActivity() {
+    private val homeViewModel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TamagotchiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val coins = homeViewModel.coins.collectAsState().value
+            HomeScreen(
+                coins = coins,
+                onFeedClick = { homeViewModel.addCoins(10) }
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TamagotchiTheme {
-        Greeting("Android")
     }
 }
