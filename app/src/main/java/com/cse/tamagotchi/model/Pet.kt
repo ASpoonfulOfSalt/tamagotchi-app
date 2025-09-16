3cb52ea828c709d8f494db4a06d76b300e0a5b51
Inphamiculus <thestrangerofrathe@gmail.com>
2025-09-16 16:42:20 -0700

    Added simple Pet class.

---
+++ app/src/main/java/com/cse/tamagotchi/model/Pet.kt
@@ -0,0 +1,20 @@
+package com.cse.tamagotchi.model
+
+data class Pet(
+    val name: String,
+    var hunger: Int = 50,
+    var water: Int = 50,
+    var happiness: Int = 50
+) {
+    fun feed(amount: Int) {
+        hunger = (hunger + amount).coerceAtMost(100)
+    }
+
+    fun giveWater(amount: Int) {
+        water = (water + amount).coerceAtMost(100)
+    }
+
+    fun play(amount: Int) {
+        happiness = (happiness + amount).coerceAtMost(100)
+    }
+}
