0d0d5401192aa67b22e2c7c8f5bc646669351c57
Daniel Nelson <thestrangerofrathe@gmail.com>
2025-10-27 21:15:26 -0700

    Feature: Added lofi (royalty free) background music with adjustable volume slider in settings.

--- app/src/main/kotlin/com/cse/tamagotchi/MainActivity.kt
+++ app/src/main/kotlin/com/cse/tamagotchi/MainActivity.kt
@@ -5,19 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import com.cse.tamagotchi.audio.BackgroundMusicManager
 import com.cse.tamagotchi.repository.UserPreferencesRepository
 import com.cse.tamagotchi.ui.navigation.AppNavRoot
 import com.cse.tamagotchi.ui.theme.TamagotchiTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var userPrefs: UserPreferencesRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val userPrefs = UserPreferencesRepository(applicationContext)
         super.onCreate(savedInstanceState)
+        userPrefs = UserPreferencesRepository(applicationContext)
+
+        // Start background music
+        BackgroundMusicManager.start(this)
+
         setContent {
             val isDarkMode by userPrefs.isDarkMode.collectAsState(initial = false)
             TamagotchiTheme(darkTheme = isDarkMode) {
                 AppNavRoot()
             }
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        BackgroundMusicManager.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        BackgroundMusicManager.start(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        BackgroundMusicManager.stop()
     }
 }
